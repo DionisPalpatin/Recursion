08ab8ed53290be868171346e4d9340eb9e1c17be
DionisPalpatin <77577723+DionisPalpatin@users.noreply.github.com>
2021-04-25 17:11:23 +0300

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -4,8 +4,6 @@ elem_was_used = list()
 
 def choice():
     global n, elem_was_used
-    
-    
     if len(elem_was_used) != n:
         for i in range(1, n + 1):
             if not i in elem_was_used:
@@ -15,8 +13,6 @@ def choice():
         for number in elem_was_used:
             print(number, end=' ')
         print()
-        
-        
     if elem_was_used != []:
         del elem_was_used[-1]
 
